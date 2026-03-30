239c272129069fbc4be550021662e50f10c214c8
yaninyzwitty <ianmwa143@gmail.com>
2026-03-30 11:57:33 +0300

    Add database configuration and connection handling
    - Introduced .gitignore to exclude .env files
    - Added database configuration to config.yaml
    - Implemented PostgreSQL connection pool in db/postgres.go
    - Updated main.go to initialize database connection
    - Enhanced config.go to include DatabaseConfig struct

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"os"
 
+	"github.com/yaninyzwitty/temporal-durable-event-pipeline/db"
 	"github.com/yaninyzwitty/temporal-durable-event-pipeline/shared/logger"
 	"github.com/yaninyzwitty/temporal-durable-event-pipeline/shared/pkg"
 )
 
 func main() {
+
+	baseCtx := context.Background()
 	// Initialize logger with a default environment
 	log := logger.New(logger.EnvDevelopment)
 
+	configPath := flag.String("config", getEnvOrDefault("SERVER_CONFIG", "config.yaml"), "path to the config file")
+
 	var config pkg.Config
-	if err := config.Load(log, "config.yaml"); err != nil {
+	if err := config.Load(log, *configPath); err != nil {
 		log.Error("failed to load config", "error", err)
 		os.Exit(1)
 	}
 
-	// Use the loaded configuration
-	log.Info("Starting server", "port", config.ServerConfig.Port, "env", config.ServerConfig.Env)
+	dbConfig := pkg.DatabaseConfig{
+		Host:     config.DatabaseConfig.Host,
+		Port:     config.DatabaseConfig.Port,
+		User:     config.DatabaseConfig.User,
+		Password: config.DatabaseConfig.Password,
+		Name:     config.DatabaseConfig.Name,
+		SSLMode:  config.DatabaseConfig.SSLMode,
+	}
+
+	// Initialize database connection pool with default options
+	pool, err := db.WaitForDB(baseCtx, dbConfig, 10, db.DefaultPoolOptions()...)
+	if err != nil {
+		log.Error("failed to connect to database", "error", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+	log.Info("successfully connected to database")
+
+}
+
+// getEnvOrDefault checks if an environment variable exists and returns its value, otherwise it returns a default value.
+func getEnvOrDefault(envKey, defaultValue string) string {
+	if value, exists := os.LookupEnv(envKey); exists {
+		return value
+	}
+	return defaultValue
 }
